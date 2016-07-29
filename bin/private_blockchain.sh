b2e5fbe03ec2088f07c711ed95838804ba42a1e9
Kevin Leng <ldh2068vip@gmail.com>
2016-07-29 10:09:47 +0800

    replace genesis flag with init

--- bin/private_blockchain.sh
+++ bin/private_blockchain.sh
@@ -2,4 +2,6 @@
 
 geth=${GETH:-geth}
 
-$geth --datadir data --genesis ./genesis.json --networkid 31415926 --rpc --rpccorsdomain "*" console
+# $geth --datadir data --genesis ./genesis.json --networkid 31415926 --rpc --rpccorsdomain "*" console 
+# flag genesis has been replaced by init sub command
+$geth  --datadir data  --networkid 31415926 --rpc --rpccorsdomain "*" console init ./genesis.json
